71cbfee6407f70029817a7c8c99305c463f21089
CliffGallant <cliff.20047@mhs.unesa.ac.id>
2022-11-20 20:11:13 +0700

    Menyelesaikan bagian User

--- home.php
+++ home.php
@@ -61,28 +61,42 @@
             </div>
             <!-- /.info-box -->
           </div>
-      </div>
-
-
-
-      
-<?php else: ?>
-	 <div class="col-12">
-          <div class="card">
-          	<div class="card-body">
-          		Welcome <?php echo $_SESSION['login_name'] ?>!
-          	</div>
-          </div>
-      </div>
-      <div class="row">
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box">
               <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
 
               <div class="info-box-content">
-                <span class="info-box-text">Total Documents</span>
+                <span class="info-box-text">Keselamatan dan Kesehatan Kerja</span>
                 <span class="info-box-number">
-                  <?php echo $conn->query("SELECT * FROM documents  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+                  <?php echo $conn->query("SELECT * FROM k3  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col --><div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Arsitektur dan Pemrograman Dasar Komputer</span>
+                <span class="info-box-number">
+                  <?php echo $conn->query("SELECT * FROM apdk  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col --><div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Distribusi Daya</span>
+                <span class="info-box-number">
+                  <?php echo $conn->query("SELECT * FROM disda  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
                 </span>
               </div>
               <!-- /.info-box-content -->
@@ -92,13 +106,58 @@
           <!-- /.col -->
       </div>
 
-      <!-- /.col -->
-      <div class="col-12 col-sm-6 col-md-3">
+
+
+      
+<?php else: ?>
+  <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            Welcome <?php echo $_SESSION['login_name'] ?>!
+          </div>
+        </div>
+      </div>
+  </div>
+  <div class="row">
+    <div class="col-12 col-sm-6 col-md-3">
+      <div class="info-box">
+      <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
+
+      <div class="info-box-content">
+        <span class="info-box-text">Total Documents</span>
+        <span class="info-box-number">
+          <?php echo $conn->query("SELECT * FROM documents  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+        </span>
+        </div>
+         <!-- /.info-box-content -->
+      </div>
+      <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <!-- /.col -->
+    <div class="col-12 col-sm-6 col-md-3">
+      <div class="info-box">
+         <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
+
+         <div class="info-box-content">
+          <span class="info-box-text">Rangkaian Listrik DC</span>
+          <span class="info-box-number">
+            <?php echo $conn->query("SELECT * FROM rldc  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+          </span>
+         </div>
+        <!-- /.info-box-content -->
+        </div>
+      <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <!-- /.col -->
+    <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box">
               <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
 
               <div class="info-box-content">
-                <span class="info-box-text">Rangkaian Listrik DC</span>
+                <span class="info-box-text">Rangkaian Listrik AC</span>
                 <span class="info-box-number">
                   <?php echo $conn->query("SELECT * FROM rldc  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
                 </span>
@@ -108,5 +167,66 @@
             <!-- /.info-box -->
           </div>
           <!-- /.col -->
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Kewirausahaan</span>
+                <span class="info-box-number">
+                  <?php echo $conn->query("SELECT * FROM documents  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Keselamatan dan Kesehatan Kerja</span>
+                <span class="info-box-number">
+                  <?php echo $conn->query("SELECT * FROM k3  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col --><div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Arsitektur dan Pemrograman Dasar Komputer</span>
+                <span class="info-box-number">
+                  <?php echo $conn->query("SELECT * FROM apdk  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col --><div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-folder"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Distribusi Daya</span>
+                <span class="info-box-number">
+                  <?php echo $conn->query("SELECT * FROM disda  where user_id = {$_SESSION['login_id']}")->num_rows; ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+      </div>
+  </div>
+
+    
           
 <?php endif; ?>
